47ce7c7748cd17984a7118d7797fcb6cb0841f2a
Vinod-Nadar-03 <162417445+Vinod-Nadar-03@users.noreply.github.com>
2024-05-16 21:06:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let color = "yellow";
+if(color == "red"){
+    console.log("Please Stop Your Vehicle");
+}
+if(color == "yellow"){
+    console.log("Go Faster or you will have to wait another 2 minutes");
+}
+if(color == "green"){
+    console.log("Go Goo Gooooo....");
+}
